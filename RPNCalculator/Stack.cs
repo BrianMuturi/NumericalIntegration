422734106924588b122746c8221b5b0bb351bbdd
Brian Muturi <muturibrian18@gmail.com>
2022-02-03 21:49:17 +0100

    Add project files.

---
+++ RPNCalculator/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalIntegration
+{
+    class Stack<T>
+    {
+        int count;
+        T[] elements;
+
+        public Stack()
+        {
+            count = 0;
+            elements = new T[10];
+        }
+
+        public void Push(T value)
+        {
+            elements[count] = value;
+            count++;
+        }
+
+        public T Peek()
+        {
+            return elements[count - 1];
+        }
+
+        public T Pop()
+        {
+            count--;
+            return elements[count];
+        }
+
+        public bool IsEmpty()
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
